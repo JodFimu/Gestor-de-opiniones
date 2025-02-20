38841be70d639a25154eb0f7ceda4f0566bc06a8
JohanMiguel <jtojin-2020591@kinal.edu.gt>
2025-02-20 11:56:53 -0600

    feat: se guardo el progreso de clase

--- src/category/category.routes.js
+++ src/category/category.routes.js
@@ -1,9 +1,10 @@
-import {createCategory} from './category.controller.js';
-import {createCategoryValidator} from '../middlewares/category-validators.js';
+import {createCategory, editCategory} from './category.controller.js';
+import {createCategoryValidator, editCategoryValidator} from '../middlewares/category-validators.js';
 import {Router} from 'express';
 
 const router = Router();
 
-router.post('/createCategory', createCategoryValidator, createCategory);
+router.post("/createCategory", createCategoryValidator, createCategory);
+router.put("/editCategory/:cid", editCategoryValidator, editCategory);
 
 export default router;
--- src/middlewares/category-validators.js
+++ src/middlewares/category-validators.js
@@ -2,11 +2,23 @@ import {body } from 'express-validator';
 import { categoryExist } from '../helpers/db-validators.js';
 import { validarCampos } from './validate-fields.js';
 import { handleErrors } from './handle-errors.js';
+import { validateJWT } from './validate-jwt.js';
+import { hasRoles } from './validate-roles.js';
 
 
 export const createCategoryValidator = [
+    validateJWT,
+    hasRoles('ADMIN_ROLE'),
     body("name").notEmpty().withMessage('El nombre es requerido'),
     body("description").notEmpty().withMessage('La descripción es requerida'),
+    body("name").custom(categoryExist),
+    validarCampos,
+    handleErrors
+]
+
+export const editCategoryValidator = [
+    validateJWT,
+    hasRoles('ADMIN_ROLE'),
     body("name").custom(categoryExist),
     validarCampos,
     handleErrors
