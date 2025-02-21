7b7992ca15ad462c21ae8dba431f0a0a91d5c9af
JodFimu <josedavidfimu@gmail.com>
2025-02-21 11:36:33 -0600

    feat: se agrego documentacion

--- src/post/post.routes.js
+++ src/post/post.routes.js
@@ -1,11 +1,94 @@
 import { Router } from "express";
-import { createPost, editPost,deletePost } from "./post.controller.js";
-import { createPostValidator, editPostValidator, deletePostValidator } from "../middlewares/post-validators.js"
+import { createPost, editPost, deletePost } from "./post.controller.js";
+import { createPostValidator, editPostValidator, deletePostValidator } from "../middlewares/post-validators.js";
 
 const router = Router();
 
-router.post("/post", createPostValidator, createPost)
-router.put("/editPost", editPostValidator, editPost)
-router.delete("/deletePost", deletePostValidator, deletePost)
+/**
+ * @swagger
+ * /post:
+ *   post:
+ *     summary: Create a new post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               auth:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Post has been created
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.post("/post", createPostValidator, createPost);
 
-export default router
+/**
+ * @swagger
+ * /editPost:
+ *   put:
+ *     summary: Edit an existing post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *               auth:
+ *                 type: string
+ *               category:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Post has been updated
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.put("/editPost", editPostValidator, editPost);
+
+/**
+ * @swagger
+ * /deletePost:
+ *   delete:
+ *     summary: Delete an existing post
+ *     tags: [Posts]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               auth:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Post has been deleted
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
+router.delete("/deletePost", deletePostValidator, deletePost);
+
+export default router;
